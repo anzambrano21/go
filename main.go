1e3bc08d747d58b780c7ee335c68ac240784625c
anzambrano21 <151884941+anzambrano21@users.noreply.github.com>
2024-07-25 01:19:23 -0400

    Actualizacion main.go

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -9,7 +10,7 @@ func f(x float64) float64  { return math.Pow(x-3, 2) }
 func df(x float64) float64 { return 2 * (x - 3) }
 
 // Implementa el algoritmo de descenso de gradiente
-func gradientDescent(x0, alpha float64, maxIterations int) (float64, float64) {
+func decensoGradianes(x0, alpha float64, maxIterations int) (float64, float64) {
 	x := x0
 	for i := 0; i < maxIterations; i++ {
 		x -= alpha * df(x)
@@ -18,16 +19,18 @@ func gradientDescent(x0, alpha float64, maxIterations int) (float64, float64) {
 }
 
 func main() {
+	var x0, alpha float64
+	var maxIterations int
 	// Entradas
 	fmt.Print("Ingrese el punto inicial (x0): ")
 	fmt.Scan(&x0)
 	fmt.Print("Ingrese el tamaño de paso (alpha): ")
 	fmt.Scan(&alpha)
 	fmt.Print("Ingrese el número máximo de iteraciones: ")
-	fmt.Scan(&maxIter)
+	fmt.Scan(&maxIterations)
 
 	// Ejecuta el algoritmo de descenso de gradiente
-	xMin, fMin := gradientDescent(x0, alpha, maxIterations)
+	xMin, fMin := decensoGradianes(x0, alpha, maxIterations)
 
 	// Resultados
 	fmt.Printf("Valor de x que minimiza la función: %.4f\n", xMin)
